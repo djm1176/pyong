34551a6eeca7a374a31d67393a03c3e31629b7b7
Moore <djm1176@live.missouristate.edu>
2019-08-20 13:20:49 -0500

    First commit

---
+++ game.py
@@ -0,0 +1,13 @@
+import pygame
+import time
+import sys
+
+SCREENWIDTH = 960
+SCREENHEIGHT = 540
+
+if not __name__ == "__main__":
+    sys.exit(0)
+
+pygame.init()
+
+gameScreen = pygame.Surface((SCREENWIDTH, SCREENHEIGHT))
