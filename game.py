bdb66b8fe3cfcd463299ff0d61e59911e52b288e
Moore <djm1176@live.missouristate.edu>
2019-08-22 11:14:55 -0500

    Added basic keyboard input

--- game.py
+++ game.py
@@ -1,13 +1,43 @@
+if not __name__ == "__main__":
+    print("Cannot run 'game.py' in this way")
+    sys.exit(0)
+
 import pygame
+from pygame import *
 import time
 import sys
 
 SCREENWIDTH = 960
 SCREENHEIGHT = 540
 
-if not __name__ == "__main__":
-    sys.exit(0)
 
 pygame.init()
 
-gameScreen = pygame.Surface((SCREENWIDTH, SCREENHEIGHT))
+display = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+
+clock = pygame.time.Clock()
+
+fps_delay = 60
+fps_delta = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            print("Closing game...")
+            pygame.quit()
+            sys.exit(0)
+            
+        elif event.type == KEYDOWN:
+            print("Key Down: ", event.key)
+        elif event.type == KEYUP:
+            print("Key Up: ", event.key)
+    
+    display.fill(pygame.Color(255, 255, 255))
+    pygame.display.update()
+    clock.tick(60)
+    
+    if fps_delta >= fps_delay:
+        fps_delta = 0
+        print("%0.2f" % clock.get_fps() + " FPS")
+    
+    fps_delta += 1
